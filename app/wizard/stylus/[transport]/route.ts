a14698582573aa37cd1ccd5272dee03141a6a07e
GianfrancoBazzani <bazzanigianfranco@gmail.com>
2025-06-27 13:45:45 +0200

    sparkles feat: Add stylus MCP.

---
+++ app/wizard/stylus/[transport]/route.ts
@@ -0,0 +1,38 @@
+import { createMcpHandler } from "@vercel/mcp-adapter";
+import { registerStylusTools } from "@ericglau/wizard-mcp/src/stylus/tools";
+import wizardMcpPackage from "@ericglau/wizard-mcp/package.json";
+
+const serverOptions = {
+  serverInfo: {
+    name: "OpenZeppelin Stylus Wizard MCP Server",
+    version: wizardMcpPackage.version,
+  },
+  capabilities: {
+    tools: {
+      listChanged: true,
+    },
+    instructions: `
+A tool for generating secure Stylus smart contracts using OpenZeppelin's standardized templates.
+It can generate and return to the client in plain text the code of ERC20, ERC721 and ERC1155 tokens contracts with configurable features.
+Outputs production-ready code following security best practices.
+`,
+  },
+};
+
+const serverConfig = {
+  basePath: "/wizard/stylus",
+  verboseLogs: true,
+  maxDuration: 60,
+};
+
+const handler = createMcpHandler(
+  async (server) => {
+    registerStylusTools(server);
+  },
+  serverOptions,
+  serverConfig
+);
+
+export const GET = handler;
+export const POST = handler;
+export const DELETE = handler;
