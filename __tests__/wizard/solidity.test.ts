b888077d2077cb070e4d5f9d2b93e70bd6f7683d
GianfrancoBazzani <bazzanigianfranco@gmail.com>
2025-07-06 20:17:56 +0200

    :tea: test: Add tests

--- __tests__/wizard/solidity.test.ts
+++ __tests__/wizard/solidity.test.ts
@@ -6,6 +6,8 @@ import {
   TEST_CLIENT_TOOLS_LIST_REQUEST,
   parseJsonData,
 } from "./common";
+import { getTitleText, getInstructionsText } from "@/wizard/prompts";
+import wizardMcpPackage from "@openzeppelin/wizard-mcp/package.json";
 
 const SOLIDITY_TOOLS_NAMES = [
   "solidity-erc20",
@@ -46,7 +48,20 @@ it("Server should initialize a client session and serve Solidity tools", async (
       const responseIitialize = await fetch(TEST_CLIENT_INITIALIZATION_REQUEST);
       const responseInitialized = await fetch(TEST_CLIENT_INITIALIZED_REQUEST);
       expect(responseInitialized.ok).toBe(true);
-      // TODO: Assert instructions and title
+
+      // Assert title, version and instructions
+      const responseInitializeText = parseJsonData(
+        await responseIitialize.text()
+      );
+      expect(getTitleText("Solidity")).toBe(
+        responseInitializeText["result"]["serverInfo"]["name"]
+      );
+      expect(wizardMcpPackage.version).toBe(
+        responseInitializeText["result"]["serverInfo"]["version"]
+      );
+      expect(getInstructionsText("Solidity")).toBe(
+        responseInitializeText["result"]["capabilities"]["instructions"]
+      );
 
       // Assert that avaiable tools are the Solidity tools
       const responseToolsList = await fetch(TEST_CLIENT_TOOLS_LIST_REQUEST);
